69396d835f003bb4eaa2be361e7f63fec4875b51
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-25 23:37:46 -0300

    :boom: fix(routes): fix product controller import

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { ProductController } from "./product/product.controller";
+import { ProductController } from "./product/product-controller";
 
 const router = Router()
 router.post('/products', new ProductController().create)
